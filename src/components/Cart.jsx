ddecc207d5d10b5bfddb71f09613b50d21f78e51
S3ck1 <seck.dv15@gmail.com>
2022-10-06 18:25:12 -0600

    few things modified

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,10 @@ import React, { useEffect } from "react";
 import { Button, ListGroup, Offcanvas } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { handleClose } from "../store/slices/show.slice";
-import { getCartProductsThunk, removeProductThunk } from "../store/slices/cart.slice";
+import {
+  getCartProductsThunk,
+  removeProductThunk,
+} from "../store/slices/cart.slice";
 import { Link, useNavigate } from "react-router-dom";
 import CheckoutModal from "./CheckoutModal";
 
@@ -10,15 +13,15 @@ const Cart = ({ getProductImg }) => {
   const show = useSelector((state) => state.show);
   const dispatch = useDispatch();
   const cartProducts = useSelector((state) => state.cart);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getCartProductsThunk());
   }, []);
 
   const removeProduct = (product) => {
-    dispatch(removeProductThunk(product.id))
-  }
+    dispatch(removeProductThunk(product.id));
+  };
 
   return (
     <Offcanvas
@@ -31,18 +34,17 @@ const Cart = ({ getProductImg }) => {
       </Offcanvas.Header>
       <ListGroup>
         {cartProducts.map((product) => (
-          <ListGroup.Item key={product.id}>
-            <Link to={`/products/${product.id}`}>
-							{product.title}
-            </Link>
-            <div className="cart-product-info">
-            </div>
-            <Button onClick={() => removeProduct(product)}>Remove</Button>
+          <ListGroup.Item className="cart-item" key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+            <div className="cart-product-info"></div>
+            <Button size="sm" onClick={() => removeProduct(product)}>
+              Remove
+            </Button>
           </ListGroup.Item>
         ))}
       </ListGroup>
       <Offcanvas.Body></Offcanvas.Body>
-      <CheckoutModal getProductImg={getProductImg}/>
+      <CheckoutModal getProductImg={getProductImg} />
     </Offcanvas>
   );
 };
