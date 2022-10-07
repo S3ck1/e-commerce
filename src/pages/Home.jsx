11fdea32112c9f6b5889d87b8154f2b6bd68b428
S3ck1 <seck.dv15@gmail.com>
2022-10-06 20:05:22 -0600

    styles added to products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ const Home = () => {
   const [categories, setCategories] = useState([]);
   const [productsFiltered, setProductsFiltered] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [sortOption, setSortOption] = useState(0);
 
   useEffect(() => {
     axios
@@ -27,7 +28,6 @@ const Home = () => {
     const filtered = productsList.filter(
       (product) => product.category.id == categoryID
     );
-    console.log(filtered);
     setProductsFiltered(filtered);
   };
 
@@ -42,6 +42,9 @@ const Home = () => {
     <div>
       <h1>Categories</h1>
       <div className="categories-nav-container">
+        <Button onClick={() => setProductsFiltered(productsList)}>
+          All
+        </Button>
         {categories.map((category) => (
           <Button key={category.id} onClick={() => filterCategory(category.id)}>
             {category.name}
@@ -51,7 +54,7 @@ const Home = () => {
       <div className="search-container">
         <InputGroup
           className="mb-3 search-product"
-          style={{ maxWidth: "600px", margin: "20px auto" }}
+          style={{ maxWidth: "700px", margin: "20px auto" }}
         >
           <Form.Control
             placeholder="Search Products"
@@ -63,18 +66,21 @@ const Home = () => {
           </Button>
         </InputGroup>
       </div>
-      <ul className="products-list">
-        {productsFiltered.map((product) => (
-          <li
-            className="product"
-            key={product.id}
-            onClick={() => navigate(`/products/${product.id}`)}
-          >
-            <h3> {product.title} </h3>
-            <img src={product.productImgs[0]} alt="" />
-          </li>
-        ))}
-      </ul>
+      <div className="products-list-container">
+        <ul className="products-list">
+          {productsFiltered.map((product) => (
+            <li
+              className="product"
+              key={product.id}
+              onClick={() => navigate(`/products/${product.id}`)}
+            >
+              <img src={product.productImgs[0]} alt="" />
+              <b>{product.title}</b>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
